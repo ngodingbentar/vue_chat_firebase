639117579c78676eb48fd1caddccb5e6a6a9e641
ngodingbentar <dewaqintoro@gmail.com>
2023-06-30 10:21:15 +0700

    updated: firebase config

--- src/db.js
+++ src/db.js
@@ -7,7 +7,8 @@ const config = {
 	projectId: process.env.VUE_APP_PROJECT_ID,
 	storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
 	messagingSenderId: process.env.VUE_APP_SENDER_ID,
-	appId: process.env.VUE_APP_APP_ID
+	appId: process.env.VUE_APP_APP_ID,
+	databaseURL: process.env.VUE_APP_DB_URL
 }
 
 const db = firebase.initializeApp(config);
